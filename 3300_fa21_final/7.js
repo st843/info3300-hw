8122098d33b4278a990b493e521c48e846386e43
Srishti Tyagi <st843@cornell.edu>
2021-12-11 19:38:17 -0500

    got up to extent

--- 3300_fa21_final/7.js
+++ 3300_fa21_final/7.js
@@ -1,9 +1,43 @@
 // ----- MAKE YOUR CHANGES BETWEEN THESE LINES FOR #7 -----
 
+// setting up svg
+const scatter = d3.select("svg#scatter");
+const scatterWidth = scatter.attr("width");
+const scatterHeight = scatter.attr("height");
+const scatterMargin = {bottom: 32, left: 32, top: 10, right: 10};
+
+const scatterChartWidth = scatterWidth - scatterMargin.left - scatterMargin.right;
+const scatterChartHeight = scatterHeight - scatterMargin.top - scatterMargin.bottom;
+
+let scatterArea = scatter.append("g")
+                         .attr("transform","translate("+scatterMargin.left+","+scatterMargin.top+")")
+                         .attr('id', 'points');
+let labels = scatter.append('g').attr('id', 'labels');
+
+const scatterData = async function() {
+    let ages = await d3.json('olympic_ages.json');
+    console.log(ages);
+
+    const parseYear = d3.timeParse("%Y");
+    ages.forEach( d => {
+        d['date'] = parseYear(d['date']).getFullYear();
+    } );
+    console.log(ages);
+
+    // extents and scales
+    const timeExtent = d3.extent(ages, d => d['date']);
+    console.log(timeExtent);
+
+    
 
 
 
 
+}
+
+
+// end of scatterData
+scatterData();
 
 
 
